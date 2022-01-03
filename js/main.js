8f0cfc12c5adb849395676b72542be06d95c326c
Muniox <muniox20@gmail.com>
2022-01-03 10:26:34 +0100

    add js and style for accordion

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const accordionElement = document.getElementsByClassName("card__question");
+
+for (let i=0;i<accordionElement.length;i++) {
+    accordionElement[i].addEventListener('click', function(){
+        this.classList.toggle('card__question--active');
+
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+    })
+    
+}
+
+
+console.log(accordionElement);
